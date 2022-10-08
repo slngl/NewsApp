1e01a4d7c3f233b312b462919d77e084fcbea9d7
SELINGUL <dselin26@gmail.com>
2022-10-08 14:31:52 +0300

    Handling Breaking News Response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/viewmodels/NewsViewModel.kt
@@ -1,9 +1,37 @@
 package com.androiddevs.mvvmnewsapp.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.data.NewsResponse
 import com.androiddevs.mvvmnewsapp.usecases.NewsRepository
+import com.androiddevs.mvvmnewsapp.utils.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init{
+        getBreakingNews("tr")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val res = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(res))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
